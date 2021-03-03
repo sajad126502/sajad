9f504de3d725ef29b0175b06a5d384c0770fbafd
sajad126502 <77586522+sajad126502@users.noreply.github.com>
2021-03-04 01:48:47 +0530

    Add files via upload

---
+++ qoutes/app-DESKTOP-FBTDV4C.js
@@ -0,0 +1,53 @@
+/*
+ function shareOnFB(){
+       var url = "https://www.facebook.com/sharer/sharer.php?u=https://yoururl.com&t=your message";
+       window.open(url, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=300,width=600');
+       return false;
+  }
+
+
+  */ 
+ var actualdata=""
+ var rand=""
+ var mdata=""
+function shareOntwitter(){
+    var url = `https://twitter.com/intent/tweet?url=${mdata}&text=${actualdata[rand].text}`;
+    TwitterWindow = window.open(url, 'TwitterWindow',width=600,height=300);
+    return false;
+ }
+console.log("welcome")
+let jdata=document.querySelector(".jdata")
+let btn=document.querySelector(".btn")
+let adata=document.querySelector(".adata")
+let img=document.querySelector(".img")
+api="https://type.fit/api/quotes"
+getdata=async ()=>{
+data=await fetch(api)
+actualdata=await data.json()
+
+}
+randomdata=()=>{
+   try{
+      
+      rand=Math.ceil(Math.random()*actualdata.length)
+      jdata.innerHTML=actualdata[rand].text
+      if(actualdata[rand].author==null)
+      mdata="Author: Unknown"
+      else
+      mdata="Author: "+actualdata[rand].author
+      adata.innerHTML=mdata
+   }
+   catch{
+      jdata.innerHTML=`<h1>something went wrong</h1>`
+   }
+}
+
+getdata().then(randomdata).catch(()=>{
+ jdata.innerText="something went wrong"
+ console.log("someeeee")
+})
+
+
+
+btn.addEventListener("click",randomdata)
+img.addEventListener("click",shareOntwitter)
